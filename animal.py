7a206a07bd4da10cadf2f422b6677700d5ad68e9
Pathiraja-D <pathirajadnew@gmail.com>
2024-02-16 15:14:56 +0530

    dog file added

--- animal.py
+++ animal.py
@@ -2,8 +2,12 @@ import sys
 def default():
     print("Hello")
 
+def dog():
+    print("Bark")
+
 def main():
-    default()
+    if sys.argv[1]=='dog':
+        print("dog")
 
 if __name__ == '__main__':
     main()
